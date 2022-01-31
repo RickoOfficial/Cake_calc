b20d2fb9a6646652e3bf0df08e151dd399ae57ae
RickoOfficial <55693888+RickoOfficial@users.noreply.github.com>
2022-01-31 23:03:39 +0400

    Add files via upload

---
+++ _nuxt/ad73d43.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{268:function(n,e,t){"use strict";t.r(e);var l=t(57),component=Object(l.a)({},(function(){var n=this,e=n.$createElement;return(n._self._c||e)("h1",[n._v("asd")])}),[],!1,null,null,null);e.default=component.exports}}]);
---
+++ _nuxt/bdffe2f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{267:function(e,n,t){"use strict";t.r(n);var l={head:{title:"Главная"}},c=t(57),component=Object(c.a)(l,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("h1",[e._v("Home")])}),[],!1,null,null,null);n.default=component.exports}}]);
